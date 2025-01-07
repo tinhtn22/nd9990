07a9b3be3ee4b6b5174022d63dcb43fba0175453
Tinh Tran Ngoc <tinhtn@fpt.com>
2025-01-07 16:12:07 +0900

    image processing

---
+++ server.js
@@ -0,0 +1,60 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+    /**************************************************************************** */
+    app.get("/filteredimage", async (req, res) => {
+      let { image_url } = req.query;
+      // 1. validate the image_url query
+      if (!image_url) {
+        return res
+          .status(422)
+          .send(`Missing required param: image_url`);
+      }
+      // 2. call filterImageFromURL(image_url) to filter the image
+      filterImageFromURL(image_url)
+        .then((result) => {
+          // 3. send the resulting file in the response
+          res.sendFile(result);
+          // 4. deletes any files on the server on finish of the response
+          res.on('finish', async () => await deleteLocalFiles([result]));
+        })
+        .catch((error) => {
+          console.log(error);
+          res.status(500).send(error);
+        })
+    });
+  //! END @TODO1
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async (req, res) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
